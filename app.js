c30b1d358c584bbdbf83cdf2eda6f45b4bf9fa6e
akshat rai <1akshat341@gmail.com>
2024-04-08 18:01:38 +0530

    add

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ const methodOverride = require('method-override');
 const passport = require('passport');
 const LocalStrategy = require('passport-local');
 const User = require('./models/user');
-
+// const dburl=process.env.DB_URL
 
 const userRoutes = require('./routes/users');
 const campgroundRoutes = require('./routes/campgrounds');
